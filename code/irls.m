8886b17c89e9df46d65cd5002be8e8fcfdf32624
syamantak <syamantakk@cse.iitb.ac.in>
2018-11-21 19:19:21 +0530

    PCA correction

--- code/irls.m
+++ code/irls.m
@@ -5,9 +5,9 @@ function X_tilda = irls(X,style_patch,patch_size,r,IRLS_itr,size)
     d = size(3);
     num_patches = (h-patch_size+1)*(w-patch_size+1);
 
-    proj_mat = style_patch.proj_mat;
-    S_eig = style_patch.S_eig;
-    mean_data = style_patch.mean_data;
+    proj_mat = style_patch{1};
+    S_eig = style_patch{2};
+    mean_data = style_patch{3};
    
     X_itr = X;
     
